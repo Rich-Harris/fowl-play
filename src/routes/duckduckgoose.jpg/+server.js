53096fc5d33c51517085e22b4f105876481dc342
Rich Harris <rich.harris@vercel.com>
2024-01-19 17:36:08 -0500

    update

--- src/routes/duckduckgoose.jpg/+server.js
+++ src/routes/duckduckgoose.jpg/+server.js
@@ -1,15 +1,8 @@
-import fs from 'node:fs';
+import duck from './duck.jpg';
+import goose from './goose.jpg';
+import { read } from '$app/server';
 
 export function GET() {
-	const bird = Math.random() < 2 / 3 ? 'duck' : 'goose';
-	const data = fs.readFileSync(
-		`src/routes/duckduckgoose.jpg/${bird}.jpg`
-	);
-
-	return new Response(data, {
-		headers: {
-			'Content-Type': 'image/jpeg',
-			'Content-Length': data.length.toString()
-		}
-	});
+	const bird = Math.random() < 2 / 3 ? duck : goose;
+	return read(bird);
 }
